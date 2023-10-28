b37a851f7bbf261d431a82e675fa29538ea0fb8a
pedro-oliveira19 <144576934+pedro-oliveira19@users.noreply.github.com>
2023-10-28 01:51:19 -0300

    Update routes.py

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -32,7 +32,7 @@ def login():
     form_criar_conta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_login' in request.form:
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode('utf-8'), form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login de {form_login.email.data} feito com sucesso.', 'alert-success')
             par_next = request.args.get('next')
@@ -43,7 +43,7 @@ def login():
         else:
             flash(f'Falha no Login. Email ou senha incorretos', 'alert-danger')
     if form_criar_conta.validate_on_submit() and 'botao_criar_conta' in request.form:
-        senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data).decode('utf-8')
         usuario = Usuario(username=form_criar_conta.username.data, email=form_criar_conta.email.data, senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
